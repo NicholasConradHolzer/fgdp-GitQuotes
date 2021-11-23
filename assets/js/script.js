b5ccc365de91de41a4b80a634ea6ba994908b06f
NicholasCH <nicholasch96@gmail.com>
2021-11-23 14:47:22 -0600

    added parse int and can now cosole log value code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
 var apiKey = "4a28d039438d8964f8454bc9ec5e51f1";
 var apiPoster= "https://www.omdbapi.com/?apikey=c1be519f&";
-var apiGenreURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=4a28d039438d8964f8454bc9ec5e51f1&language=en-US`;
-// var apiTemp= ;
 var genreEl = document.getElementById("Genre");
 var subGenreEl = document.getElementById("Subgenre");
-// var timeEl= document.getElementById("Timelimit")
-var clickbtn = document.querySelector("#randmovie");
+var clickbtn = document.getElementById("randmovie");
 
 var parameters = function (){
-    var genreSelected = genreEl.options[genreEl.selectedIndex].val;
-    var subGenreSelected = subGenreEl.options[subGenreEl.selectedIndex].text;
-    // var timeLimit= timeEl.options[timeEl.selectedIndex].text
+    var genreSelected = genreEl.options[genreEl.selectedIndex].value;
+    var subGenreSelected = ("," + subGenreEl.options[subGenreEl.selectedIndex].value);
+    parseInt(genreSelected , subGenreSelected);
+    if (subGenreSelected===0) {
+        var subGenreSelected = ""
+    }
+    var apiGenreURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + 
+    "&with_genres=" + genreSelected + subGenreSelected;
+
     console.log(genreSelected)
     console.log(subGenreSelected);
-    // console.log(timeLimit);
 }
 
 clickbtn.addEventListener("click", parameters);
