4ffc11f24e73a7adeba7933d518516e259108355
Cody-Junier <90635812+Cody-Junier@users.noreply.github.com>
2021-11-23 15:36:37 -0600

    Psuedo-code

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,8 +19,17 @@ var parameters = function (event){
     return res.json();
     })
     .then(function (data){
-        console.log(data.total_results);
         console.log(data);
+        var resultsParam= data.total_results
+        console.log(resultsParam);
+        // Use resultsParam to randomly select a movie
+        // Max results per page = 20
+        // we will want to divide resultsParam by 20( example 35/20= 1.75), drop the decimal values, then add 1 to get the page number
+        // once we have a page number, we want to randomly select a number 1-20, subtract 1, (or randInt 0-19) to get a random title
+        // Take random page, add it to api search/call with random movie position in array
+        // call api again with that info
+        // get title from second api call, pass it through second api (omdb) to get poster
+        // Populate title, tagline (mayber other info?) from first api, populate poster with second api.
     })
     .catch(function (err) {
         console.error(err);
