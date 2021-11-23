375b392a956c245245ea746dded2fe60aae61013
NicholasConradHolzer <89279968+NicholasConradHolzer@users.noreply.github.com>
2021-11-23 14:49:05 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
 var apiKey = "4a28d039438d8964f8454bc9ec5e51f1";
 var apiPoster= "https://www.omdbapi.com/?apikey=c1be519f&";
-// var apiURL = ;
-// var apiTemp= ;
 var genreEl = document.getElementById("Genre");
 var subGenreEl = document.getElementById("Subgenre");
-// var timeEl= document.getElementById("Timelimit")
+var clickbtn = document.getElementById("randmovie");
 
-
-function parameters(){
+var parameters = function (){
     var genreSelected = genreEl.options[genreEl.selectedIndex].value;
-    var subGenreSelected = subGenreEl.options[subGenreEl.selectedIndex].value;
-    // var timeLimit= timeEl.options[timeEl.selectedIndex].text
+    var subGenreSelected = ("," + subGenreEl.options[subGenreEl.selectedIndex].value);
+    parseInt(genreSelected , subGenreSelected);
+    if (subGenreSelected===0) {
+        var subGenreSelected = ""
+    }
+    var apiGenreURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + 
+    "&with_genres=" + genreSelected + subGenreSelected;
+
     console.log(genreSelected)
     console.log(subGenreSelected);
-    // console.log(timeLimit);
 }
+
+clickbtn.addEventListener("click", parameters);
